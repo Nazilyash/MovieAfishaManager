dfdb34839f3049e84afbb56d65e9fad784b95d1e
misharka <fareshta88@gmail.com>
2021-05-05 16:28:41 +0300

    Classes of Afisha added

---
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -0,0 +1,41 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Movie;
+
+public class AfishaManager {
+    private Movie[] movies = new Movie[0];
+    private int moviesAmountInFeed = 10;
+
+    public Movie[] getMovies() {
+        return movies;
+    }
+
+    public AfishaManager() {
+    }
+
+    public AfishaManager(int moviesAmountInFeed) {
+        this.moviesAmountInFeed = moviesAmountInFeed;
+    }
+
+    public void addToFeed(Movie movie) {
+        int length = movies.length + 1;
+        Movie[] tmp = new Movie[length];
+        System.arraycopy(movies, 0, tmp, 0, movies.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = movie;
+        movies = tmp;
+    }
+
+    public Movie[] showMoviesFeed() {
+        int length = moviesAmountInFeed;
+        if (moviesAmountInFeed > movies.length) {
+            length = movies.length;
+        }
+        Movie[] result = new Movie[length];
+        for (int i = 0; i < result.length; i++) {
+            int index = movies.length - i - 1;
+            result[i] = movies[index];
+        }
+        return result;
+    }
+}
